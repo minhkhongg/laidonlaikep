ceaad4f19936566c94fadf3904b75ec649408d82
minhkhongg <maitramnguyen280100@gmail.com>
2026-06-16 11:20:42 +0700

    Update app title to include author name

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Tiêu đề ứng dụng
-st.title("💰 Ứng dụng tính tiền gửi tiết kiệm")
+st.title("💰 Ứng dụng tính tiền gửi tiết kiệm_ Nguyễn Mai Trâm")
 
 # Nhập dữ liệu
 C = st.number_input(
